7bedcf26e70e2ef34cba601a2bdb8163820fb258
lighten lee <gxli2988@gmail.com>
2024-03-13 08:39:15 +0800

    fix:黑马表单校验

---
+++ src/pages-sub/hmtest.vue
@@ -0,0 +1,56 @@
+<!-- 使用 type="home" 属性设置首页，其他页面不需要设置，默认为page；推荐使用json5，更强大，且允许注释 -->
+<route lang="json5">
+{
+  style: {
+    navigationBarTitleText: '黑马测试表单校验功能（setup）',
+  },
+}
+</route>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// 表单数据
+const form = ref({
+  receiver: '', // 收货人
+})
+
+// 定义校验规则
+const rules: UniHelper.UniFormsRules = {
+  receiver: {
+    rules: [{ required: true, errorMessage: '请输入收货人姓名' }],
+  },
+}
+
+// 表单组件实例
+const formRef = ref<UniHelper.UniFormsInstance>()
+
+// 提交表单
+const onSubmit = async () => {
+  try {
+    // 表单校验
+    await formRef.value?.validate?.()
+    // 成功提示
+    uni.showToast({ icon: 'success', title: '添加成功' })
+    // 返回上一页
+    setTimeout(() => {
+      uni.navigateBack()
+    }, 400)
+  } catch (error) {
+    uni.showToast({ icon: 'error', title: '请填写完整信息' })
+  }
+}
+</script>
+
+<template>
+  <view class="content">
+    <uni-forms :rules="rules" :model="form" ref="formRef">
+      <!-- 表单内容 -->
+      <uni-forms-item name="receiver" class="form-item">
+        <text class="label">收货人</text>
+        <input class="input" placeholder="请填写收货人姓名" v-model="form.receiver" />
+      </uni-forms-item>
+    </uni-forms>
+  </view>
+  <!-- 提交按钮 -->
+  <button @tap="onSubmit" class="button">保存并使用</button>
+</template>
